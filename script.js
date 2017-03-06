10f4bbde5a4346879231f10a036537680684a1c1
valtterilipsanen <valtteri.lipsanen@aalto.fi>
2017-03-06 17:49:13 +0200

    Puolipisteitä

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 
-var randomNumber = 0
-var minimum = parseInt(1)
-var maximum = parseInt(10)
+var randomNumber = 0;
+var minimum = parseInt(1);
+var maximum = parseInt(10);
 
 function getRandomInteger( min, max ){
     var difference = max - min;
     
     var number = parseInt(Math.round(Math.random() * difference + min));
     
-    return number
+    return number;
 }
 
 function setNumber(number){
-    randomNumber = number
+    randomNumber = number;
 }
 
 function compareNumber( first, second ){
-    setNumber(getRandomInteger(minimum, maximum))
-    return(first == second)
+    setNumber(getRandomInteger(minimum, maximum));
+    return(first == second);
 }
 
 function guessTheNumber(){
-    var number = Number(document.getElementById("number").value)
+    var number = Number(document.getElementById("number").value);
     
     if(Number.isInteger(number) && (number >= minimum && number <= maximum)){ 
         if(compareNumber(number, randomNumber)){
-         window.alert("You guessed right!")
+         window.alert("You guessed right!");
         } else {
-         window.alert("You guessed wrong!")    
+         window.alert("You guessed wrong!");   
         }
     } else {
-         window.alert("Given number is invalid") 
+         window.alert("Given number is invalid"); 
     }   
 }
  
 
 window.onload = function(){
-    setNumber(getRandomInteger(minimum, maximum))  
-}
+    setNumber(getRandomInteger(minimum, maximum));
+};
 
 
 
